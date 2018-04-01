14c9cc656e361118b53ac330b7a06080e8e5a1d5
AnnSamoluk <annsamoluk@mail.ru>
2018-04-01 13:01:19 +0300

    create test repositorij

---
+++ Lesson7WebBrowser/Lesson7WebBrowser/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lesson7WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 70);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(545, 258);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(537, 232);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(467, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(461, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 340);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lesson7WebBrowser/Lesson7WebBrowser/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson7WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        string homePage = @"https://www.google.com/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabControl1.TabPages[0].Text = "Google   ";
+            tabControl1.TabPages[1].Text = "+";
+            tabControl1.DrawMode = TabDrawMode.OwnerDrawFixed;
+            tabControl1.DrawItem += TabControl1_DrawItem;
+            tabControl1.MouseClick += TabControl1_MouseClick;
+
+            WebBrowser webBrowser = new WebBrowser();
+            webBrowser.Dock = DockStyle.Fill;
+            tabControl1.TabPages[0].Controls.Add(webBrowser);
+            webBrowser.Navigate(homePage);
+        }
+
+        private void TabControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (tabControl1.SelectedIndex == tabControl1.TabPages.Count - 1)
+            {
+                TabPage newPage = new TabPage("+");
+                tabControl1.TabPages.Add(newPage);
+
+                TabPage current = tabControl1.TabPages[tabControl1.SelectedIndex];
+                WebBrowser newWeb = new WebBrowser();
+                newWeb.Dock = DockStyle.Fill;
+                current.Controls.Add(newWeb);
+                newWeb.Navigate(homePage);
+                current.Text = newWeb.DocumentTitle;
+            }
+            else
+            {
+
+                Rectangle r = tabControl1.GetTabRect(tabControl1.SelectedIndex);
+                Rectangle close = new Rectangle(r.X + r.Width - 12, r.Y + 2, 10, 10);
+                if (close.Contains(e.Location))
+                {
+                    // tabControl1.TabPages.Remove(tabControl1.TabPages[tabControl1.SelectedIndex]);
+                    MessageBox.Show("Test");
+                }
+            }
+        }
+        private void TabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Rectangle r = e.Bounds;
+            Rectangle close = new Rectangle(r.X + r.Width - 12, r.Y + 2, 10, 10);
+          
+            Graphics g = e.Graphics;
+            string text = tabControl1.TabPages[e.Index].Text;
+            g.DrawString(text, e.Font, new SolidBrush(Color.Blue), r);
+
+            if (e.Index == tabControl1.TabCount - 1)
+                return;
+            g.DrawImage(Image.FromFile(@"D:\ШАГ\forms\ПрикладиЗКласу\Lesson7WebBrowser\delete.ico"), close);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            WebBrowser web = (WebBrowser)tabControl1.TabPages[tabControl1.SelectedIndex].Controls[0];
+            web.Navigate(url);
+            tabControl1.TabPages[tabControl1.SelectedIndex].Text = web.DocumentTitle;
+        }
+    }
+}
